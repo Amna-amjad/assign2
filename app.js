1d850fcc996d54e308d9a26bc273b82c167cb746
Amna-amjad <amnakhan8685@gmail.com>
2022-09-04 15:06:52 +0100

    work complete

---
+++ app.js
@@ -0,0 +1,44 @@
+//  Create nested array and nested object...
+//Copy the nested value(s) (using the SPREAD OPERATOR) and then store it in a separate variable(s).
+
+
+
+//  Nested Array
+
+// let arr1 = [
+//   [1, 2, 3],
+//   [4, 5, 6],
+//   [7, 8, 9]
+// ];
+
+// let arr2 = [...arr1[2]];
+// console.log(arr2);
+
+//  Nested Objects
+
+
+// let obj = {
+//     name : {
+//         firstName : "abc",
+//         lastName : "xyz"
+//     },
+//     address : {
+//         city : "Karachi",
+//         country : "Pakistan"
+//     }
+
+// };
+
+// let obj2 = {...obj.address}
+// console.log(obj2);
+
+
+
+
+
+
+
+
+
+
+
